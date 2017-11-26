d068fa6264ef2cd067a6943d2e6fef5ec519de81
azetor <adam.a.szymczak@gmail.com>
2017-11-27 00:20:34 +0100

    okhttp and url conn java.lang

--- app/src/main/java/com/mobilemonkeysoftware/mjpegtest/MainActivity.kt
+++ app/src/main/java/com/mobilemonkeysoftware/mjpegtest/MainActivity.kt
@@ -6,10 +6,10 @@ import android.util.Log
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.io.BufferedInputStream
 import java.math.BigInteger
-import java.net.HttpURLConnection
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,40 +20,34 @@ class MainActivity : AppCompatActivity() {
         Observable
                 .create<ByteArray> {
                     try {
-//                        val md5 = MessageDigest.getInstance("MD5")
-//
-//                        val response = OkHttpClient()
-//                                .newCall(
-//                                        Request.Builder()
-//                                                .url("http://iris.not.iac.es/axis-cgi/mjpg/video.cgi?resolution=320x240")
-//                                                .addHeader("Cache-Control", "no-cache")
-//                                                .build())
-//                                .execute()
-//                        Log.i("XXX", response.toString())
-//                        var rIs = BufferedInputStream(response.body()?.source()?.inputStream())
+
+                        val response = OkHttpClient()
+                                .newCall(
+                                        Request.Builder()
+                                                .url("http://iris.not.iac.es/axis-cgi/mjpg/video.cgi?resolution=320x240")
+                                                .addHeader("Cache-Control", "no-cache")
+                                                .build())
+                                .execute()
+                        Log.i("XXX", response.toString())
+                        var bis = BufferedInputStream(response.body()?.source()?.inputStream())
+                        do {
+                            var array = ByteArray(64)
+                            bis.read(array)
+                            Log.i("XXX", BigInteger(1, array).toString(16))
+                            it.onNext(ByteArray(0))
+                        } while (true)
+
+
+//                        // https://github.com/perthcpe23/android-mjpeg-view/blob/master/mjpegviewer/src/main/java/com/longdo/mjpegviewer/MjpegView.java
+//                        var con = URL("http://iris.not.iac.es/axis-cgi/mjpg/video.cgi?resolution=320x240").openConnection() as HttpURLConnection
+//                        con.doInput = true
+//                        con.connect()
+//                        val bis = BufferedInputStream(con.inputStream)
 //                        do {
-//                            rIs.use {
-//                                var array = ByteArray(10000)
-//                                it.read(array)
-//                                Log.i("XXX", BigInteger(1, md5.digest(array)).toString(16))
-//                            }
-//
-//                            it.onNext(ByteArray(0))
+//                            var array = ByteArray(64)
+//                            bis.read(array)
+//                            Log.i("XXX", BigInteger(1, array).toString(16))
 //                        } while (true)
-
-
-                        // https://github.com/perthcpe23/android-mjpeg-view/blob/master/mjpegviewer/src/main/java/com/longdo/mjpegviewer/MjpegView.java
-                        var con = URL("http://iris.not.iac.es/axis-cgi/mjpg/video.cgi?resolution=320x240").openConnection() as HttpURLConnection
-                        con.doInput = true
-                        con.connect()
-
-                        BufferedInputStream(con.inputStream).use {
-                            do {
-                                var array = it.readBytes(64)
-                                Log.i("XXX", BigInteger(1, array).toString(16))
-                            }   while (true)
-                        }
-
                         it.onComplete()
                     } catch (e: Exception) {
                         it.onError(e)
